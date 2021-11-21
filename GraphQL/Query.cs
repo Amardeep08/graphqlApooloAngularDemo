27c329acb22f60e816c6be2522ff39ce2ee06a6c
unknown <amardeep.professional@gmail.com>
2021-11-21 06:41:00 -0800

    initial commit folders

---
+++ GraphQL/Query.cs
@@ -0,0 +1,75 @@
+using System.Linq;
+using CommanderGQL.Data;
+using CommanderGQL.Models;
+using HotChocolate;
+using HotChocolate.Data;
+
+namespace CommanderGQL.GraphQL
+{
+    /// <summary>
+    /// Represents the queries available.
+    /// </summary>
+    [GraphQLDescription("Represents the queries available.")]
+    public class Query
+    {
+        /// <summary>
+        /// Gets the queryable <see cref="Platform"/>.
+        /// </summary>
+        /// <param name="context">The <see cref="AppDbContext"/>.</param>
+        /// <returns>The queryable <see cref="Platform"/>.</returns>
+        [UseDbContext(typeof(AppDbContext))]
+        [UseFiltering]
+        [UseSorting]
+        
+        [GraphQLDescription("Gets the queryable platform.")]
+        public IQueryable<Platform> GetPlatform([ScopedService] AppDbContext context)
+        {
+            return context.Platforms;
+        }
+
+        /// <summary>
+        /// Gets the queryable <see cref="Command"/>.
+        /// </summary>
+        /// <param name="context">The <see cref="AppDbContext"/>.</param>
+        /// <returns>The queryable <see cref="Command"/>.</returns>
+        [UseDbContext(typeof(AppDbContext))]
+        [UseFiltering]
+        [UseSorting]
+        [GraphQLDescription("Gets the queryable command.")]
+        public IQueryable<Command> GetCommand([ScopedService] AppDbContext context)
+        {
+            return context.Commands;
+        }
+
+        /// <summary>
+        /// Gets the queryable <see cref="Command"/>.
+        /// </summary>
+        /// <param name="context">The <see cref="AppDbContext"/>.</param>
+        /// <returns>The queryable <see cref="Command"/>.</returns>
+        [UseDbContext(typeof(AppDbContext))]
+        [UseFiltering]
+        [UseSorting]
+        [GraphQLDescription("Gets the queryable command.")]
+        public IQueryable<Command> UpdateCommand([ScopedService] AppDbContext context,Command command)
+        {
+            return context.Commands;
+        }
+
+              /// <summary>
+        /// Gets the queryable <see cref="Platform"/>.
+        /// </summary>
+        /// <param name="context">The <see cref="AppDbContext"/>.</param>
+        /// <returns>The queryable <see cref="Platform"/>.</returns>
+        [UseDbContext(typeof(AppDbContext))]
+        [UseFiltering]
+        [UseSorting]
+        
+        [GraphQLDescription("Gets the queryable platform.")]
+        public IQueryable<Platform> UpdatePlatform([ScopedService] AppDbContext context,Platform platform)
+        {
+            return context.Platforms;
+        }
+
+        
+    }
+}
